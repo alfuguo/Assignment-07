0f6502cf7e2405b7640349a4396a7225e5ff8b7f
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-03-28 18:00:44 -0400

    updating

---
+++ src/main/java/CustomArrayList.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Object[] items = new Object[10];								
+	int size = 0;
+	
+	@Override
+	public boolean add(T item) throws IndexOutOfBoundsException{
+		if (size == items.length) {
+			items = Arrays.copyOf(items, items.length * 2);
+		}
+		items[size] = item;
+		size++;
+		return true;
+	}
+
+
+	@Override
+	public int getSize() {
+
+		return size;
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) {
+			return (T) items[index];
+	}
+	
+}
